11545a2e505b75295c7cc00d4a72d463a3da0cc6
John Anderson <jra.hockey4@gmail.com>
2015-12-12 14:19:39 -0600

    Fixing authors

---
+++ Bounded-Dequeue.git/gitHubSavior.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="John@Johns-MacBook-Pro-2.local"
+CORRECT_NAME="John Anderson"
+CORRECT_EMAIL="jra.hockey4@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
